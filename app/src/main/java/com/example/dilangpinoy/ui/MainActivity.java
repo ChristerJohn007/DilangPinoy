2e42f6bb87220d57a8fcb497487477e70e142265
ChristerJohn007 <cjbgabutero@gmail.com>
2020-03-05 13:39:31 +0800

    JardoPC assets fix

--- app/src/main/java/com/example/dilangpinoy/ui/MainActivity.java
+++ app/src/main/java/com/example/dilangpinoy/ui/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends BaseActivity {
         final ValueAnimator animator = ValueAnimator.ofFloat(0.0f, 1.0f);
         animator.setRepeatCount(ValueAnimator.INFINITE);
         animator.setInterpolator(new LinearInterpolator());
-        animator.setDuration(100000L);
+        animator.setDuration(80000L);
         animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
